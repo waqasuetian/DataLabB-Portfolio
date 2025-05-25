a7a1d2eb225a56c0d14bcf35d1561b23474b9c74
waqasuetian <97124934+waqasuetian@users.noreply.github.com>
2025-05-26 01:56:10 +0500

    Details.js

--- src/Details.js
+++ src/Details.js
@@ -4,26 +4,44 @@ let Details = [
     Title: "Crane GPT",
     Description:
       "Introducing CraneGPT, our cutting-edge chatbot designed to revolutionize crane operations. Leveraging the power of Langchain technology and the intelligence of the LAMA-2 language model, CraneGPT becomes your crane expert on demand. It seamlessly interprets crane manuals, providing expert answers to ensure safe and efficient crane operations. Powered by VectorDB for embeddings, CraneGPT offers precision and reliability, enhancing your crane-related queries. Unlock the future of crane management with CraneGPT - your digital partner for superior crane expertise and productivity.",
-    Keywords: "Langchain, Large Language Model (LAMA-2), VectorDB",
+    Keywords: ["Langchain", "Large Language Model (LAMA-2)", "VectorDB"],
     Image: "/Pictures/CraneGPT.webp",
     Challenges: [
-      "Interpreting various crane manuals with different terminologies and structures",
+      "Interpreting various crane manuals with different terminologies and structures.",
       "Ensuring the accuracy and reliability of the answers provided.",
-      "Implementing a user-friendly interface for seamless interactions",
+      "Implementing a user-friendly interface for seamless interactions.",
     ],
     Tools: [
       "Langchain for integrating language model capabilities.",
       "LAMA-2, a Large Language Model, for understanding and generating human-like text.",
       "VectorDB for storing and retrieving vector embeddings efficiently.",
-      "ReactJs for front end development.",
+      "ReactJs for front-end development.",
     ],
   },
   {
     id: 2,
+    Title: "Speaker Verification",
+    Description:
+      "Introducing Speaker Verification, our advanced system for speaker identity verification leveraging cutting-edge AI. This system ensures accurate and secure verification of speaker identity in various applications using voice data.",
+    Keywords: ["Speaker Verification", "AI", "Voice Recognition"],
+    Image: "/Pictures/speakerVerification.webp",
+    Challenges: [
+      "Dealing with variations in voice patterns across different speakers.",
+      "Ensuring real-time verification with high accuracy.",
+      "Implementing a secure and reliable identity verification system.",
+    ],
+    Tools: [
+      "Voice processing models for identifying speaker patterns.",
+      "Machine Learning algorithms for identity verification.",
+      "ReactJs for front-end development.",
+    ],
+  },
+  {
+    id: 3,
     Title: "PashtoLexi",
     Description:
       "Our approach to creating a Pashto language thesaurus involved a combination of manual and automated methods. We analyzed Pashto texts manually and compiled lists of synonyms and related words. We also used natural language processing (NLP) techniques to extract and classify words based on their semantic similarity. To train the word2vec model, we used a Pashto corpus and evaluated it on a Pashto journals dataset. Our proposed model outperformed all existing approaches and produced reasonable results.",
-    Keywords: "NLTK and spaCy, WordNet",
+    Keywords: ["NLTK", "spaCy", "WordNet"],
     Image: "/Pictures/PashtoLexi.png",
     Challenges: [
       "Manual analysis of Pashto texts required extensive labor and time.",
@@ -32,21 +50,21 @@ let Details = [
     ],
     Tools: [
       "NLTK and spaCy for natural language processing tasks.",
-      "Train Word2Vec model from scratch for generating the embeddings of pashto text.",
+      "Train Word2Vec model for generating Pashto text embeddings.",
       "Cosine Similarity for accuracy metric.",
       "WordNet for lexical database support.",
     ],
   },
   {
-    id: 3,
+    id: 4,
     Title: "NeuroGensis",
     Description:
-      "We have used the Deep Convolutional Generative Adversarial Networks (DCGAN) machine learning algorithm to generate the fake samples. In this research-oriented work, we extracted the salient features of DCGAN and practically demonstrated and implemented it to generate the fake scans of the brain tumors and found it to outperform other types of generative adversarial networks (GANs).",
-    Keywords: "PyTorch, Deep Learning, Medical Imaging",
+      "We have used the Deep Convolutional Generative Adversarial Networks (DCGAN) machine learning algorithm to generate fake samples. In this research-oriented work, we extracted salient features of DCGAN and implemented it to generate fake scans of brain tumors, outperforming other types of GANs.",
+    Keywords: ["PyTorch", "Deep Learning", "Medical Imaging"],
     Image: "/Pictures/NeuroGenis.webp",
     Challenges: [
-      "Extracting salient features of DCGAN effectively for generating realistic fake scans.",
-      "Ensuring that the generated scans are indistinguishable from real scans for practical applications.",
+      "Extracting salient features of DCGAN for generating realistic fake scans.",
+      "Ensuring that the generated scans are indistinguishable from real scans.",
       "Outperforming other GAN models in terms of quality and realism.",
     ],
     Tools: [
@@ -55,32 +73,32 @@ let Details = [
     ],
   },
   {
-    id: 4,
+    id: 5,
     Title: "TenantTalker",
     Description:
-      "We utilized the RASA framework, along with GPT3 integration, to design and develop an interactive chatbot. Our chatbot operates on a client-server model, allowing for real-time response through the RASA framework and ChatGPT. The application has the capability to answer close questions as well as open-ended queries, providing users with a natural and engaging conversational experience. We implemented intent classification through the RASA framework, allowing the chatbot to interactively respond to the user with multiple options to sort out the problem at hand.",
-    Keywords: "RASA framework, GPT-3, chatbot",
+      "We utilized the RASA framework along with GPT-3 integration to design and develop an interactive chatbot. Our chatbot operates on a client-server model, allowing real-time response through the RASA framework and GPT-3. The chatbot answers close-ended as well as open-ended queries, offering a natural conversational experience.",
+    Keywords: ["RASA Framework", "GPT-3", "Chatbot"],
     Image: "/Pictures/TenantTalker.webp",
     Challenges: [
-      "Ensuring real-time responses in the client-server model.",
+      "Ensuring real-time responses in a client-server model.",
       "Implementing effective intent classification for interactive responses.",
-      "Fine-tuning the GPT3 model for various parameters and problem sets.",
+      "Fine-tuning the GPT-3 model for various parameters and problem sets.",
     ],
     Tools: [
-      "RASA framework for building the chatbot and implementing intent classification.",
-      "GPT-3 for integrating large-scale language model capabilities.",
+      "RASA framework for building the chatbot.",
+      "GPT-3 for large-scale language model capabilities.",
     ],
   },
   {
-    id: 5,
+    id: 6,
     Title: "PunctuationCorrector",
     Description:
-      "We implemented Bidirectional Encoder Representations from Transformer (BERT), a powerful deep learning algorithm. Our approach is similar to that of Grammarly software and is designed to correct punctuation text datasets, which are publicly available. We built a deep feed-forward neural network with 11 dense layers and utilized ReLu activation function and an optimizer AdamW for the output.",
-    Keywords: "PyTorch, deep learning, Keras, NLTK, data preprocessing",
+      "We implemented Bidirectional Encoder Representations from Transformers (BERT), a powerful deep learning algorithm similar to Grammarly software, to correct punctuation in text datasets. We built a deep feed-forward neural network with 11 dense layers, using the ReLu activation function and AdamW optimizer.",
+    Keywords: ["BERT", "Deep Learning", "Text Correction"],
     Image: "/Pictures/PunctuationCorrector.png",
     Challenges: [
       "Correcting punctuation in text datasets with high accuracy.",
-      "Implementing a deep learning model with a sufficient number of layers for effective learning.",
+      "Implementing a deep learning model with sufficient layers for effective learning.",
       "Choosing the right activation function and optimizer for the model.",
     ],
     Tools: [
@@ -90,11 +108,11 @@ let Details = [
     ],
   },
   {
-    id: 6,
+    id: 7,
     Title: "Breast Cancer Disease Detection",
     Description:
-      "We used the famous dataset downloaded from the UCI repository challenge and performed exploratory and classification analysis on the Breast Cancer dataset. We conducted extensive cleaning and data preprocessing tasks using pandas and NumPy and visualized the data using seaborn and Matplotlib. Using the ANN model, we achieved an accuracy of 95%, which was further improved up to 99% using k-fold Cross-validation.",
-    Keywords: "PyTorch, deep learning, Keras, data preprocessing",
+      "We used a dataset from the UCI repository and performed exploratory and classification analysis on the Breast Cancer dataset. After extensive cleaning and preprocessing, we achieved 95% accuracy using an ANN model, which was further improved to 99% with k-fold Cross-validation.",
+    Keywords: ["ANN", "Breast Cancer Detection", "Deep Learning"],
     Image: "/Pictures/BreastCancerDisease.png",
     Challenges: [
       "Cleaning and preprocessing the dataset effectively.",
@@ -108,44 +126,86 @@ let Details = [
     ],
   },
   {
-    id: 7,
+    id: 8,
     Title: "Clinical Decision Support System",
     Description:
-      "The Clinical Decision Support System aids young doctors in diagnosing diseases based on symptoms, especially when common symptoms overlap with multiple conditions. Data sourced from 'Habif’s Clinical Dermatology' by James G. Dinulos is annotated, converted into a CSV format, and preprocessed for compatibility with NLP-based Large Language Models like ClinicalBERT, Bio ClinicalBERT, and PubMedBert. The fine-tuned model is integrated into a Flutter mobile app, allowing doctors to input symptoms and receive predictions for the top five possible diseases along with their probabilities.",
-    Keywords: "NLP, ClinicalBERT, PubMedBert, BioClinicalBERT",
-    Image: "Clinical Decision Support System.mp4",
+      "The Clinical Decision Support System aids young doctors in diagnosing diseases based on symptoms. Using a dataset from 'Habif’s Clinical Dermatology,' we trained ClinicalBERT, BioClinicalBERT, and PubMedBERT models, which were integrated into a Flutter mobile app for disease prediction based on symptoms.",
+    Keywords: ["ClinicalBERT", "BioClinicalBERT", "NLP"],
+    Image: "/Pictures/cdss.mp4",
     Challenges: [
-      "Delicate process due to variations in terminology and context.",
+      "Handling variations in terminology and context.",
       "Manual identification of relevant symptoms for each disease.",
-      "Ensuring precision for each specific disease.",
-      "Time-consuming but essential.",
-      "Critical for creating a reliable dataset.",
+      "Ensuring precision in disease prediction.",
     ],
     Tools: [
-      "Flutter for Developing the mobile application.",
+      "Flutter for developing the mobile application.",
       "Python for backend development.",
       "Latex for documentation.",
     ],
   },
   {
-    id: 8,
+    id: 9,
     Title:
-      "Multi Source Data Fusion for Cost Effective Wheat Crop Yield Prediction",
+      "Multi Source Data Fusion for Cost-Effective Wheat Crop Yield Prediction",
     Description:
-      "The aim of this research-based project is to develop an integrated system for precise and context-aware yield forecasts in Faisalabad, using multi-source data that includes remote sensing, meteorological, and soil data, supporting sustainable agricultural practices and food security.",
-    Keywords: "Remote Sensing, Meteorological Data, Soil Data",
-    Image: "Crop Yeild.mp4",
+      "This research project focuses on developing an integrated system for precise and context-aware wheat yield forecasts in Faisalabad using multi-source data, including remote sensing, meteorological, and soil data, to support sustainable agriculture and food security.",
+    Keywords: ["Remote Sensing", "Meteorological Data", "Soil Data"],
+    Image: "/Pictures/yield.mp4",
     Challenges: [
-      "Traditional methods for yield prediction rely on ground-truthing techniques.",
-      "These techniques can be expensive and time-consuming.",
+      "Traditional yield prediction methods are expensive and time-consuming.",
+      "Integrating multiple data sources for accurate yield prediction.",
     ],
     Tools: [
       "MERN Stack for developing the web application.",
-      "Python for model development",
+      "Python for model development.",
       "Jupyter Notebook for data analysis and visualization.",
-      "VSC for code development.",
     ],
   },
+  {
+    id: 10,
+    Title: "Amazon Product Sentiment Analyzer",
+    Description:
+      "This project involves performing sentiment analysis on a dataset of Amazon product reviews. The solution includes preprocessing reviews, training a model, and deploying it as a web application. Users can input review sentences, and the model outputs the predicted sentiment.",
+    Keywords: ["Sentiment Analysis", "BiLSTM", "Streamlit"],
+    Image: "/Pictures/Amazon.jpeg",
+    Challenges: [
+      "Preprocessing reviews for sentiment classification.",
+      "Handling out-of-vocabulary words and padding.",
+      "Designing a BiLSTM model for sentiment classification.",
+      "Deploying the model in a user-friendly web application.",
+    ],
+    Tools: [
+      "PyTorch for building and training the BiLSTM model.",
+      "Streamlit for deploying the web application.",
+      "Matplotlib and NumPy for visualizing sentiment results.",
+      "Pickle for loading pre-built vocabulary for encoding text inputs.",
+    ],
+  },
+  {
+  id: 11,
+  Title: "AI-powered Dental DICOM Annotation System",
+  Description:
+    "This project involves developing an AI-powered annotation system for dental DICOM scans to automatically identify and segment anatomical features such as nerves, roots, and enamel. Using a U-Net-based deep learning architecture, the solution enhances diagnostic accuracy and streamlines radiological workflows by delivering precise, pixel-level segmentation, along with 3D rendering for clinical validation.",
+  Keywords: ["Dental AI", "U-Net", "DICOM", "Segmentation", "3D Visualization"],
+  Image: "/Pictures/dicom.jpeg",
+  Challenges: [
+    "Training a U-Net model for accurate segmentation on dental-specific DICOM data.",
+    "Developing a full preprocessing and postprocessing pipeline for DICOM files.",
+    "Handling volumetric data and implementing suitable augmentation strategies.",
+    "Integrating 3D rendering tools for clinical visualization and verification.",
+    "Ensuring annotation outputs are compatible with professional dental tools."
+  ],
+  Tools: [
+    "Python, NumPy, OpenCV for image and data processing.",
+    "PyTorch for implementing and training the U-Net segmentation model.",
+    "pydicom for DICOM data handling and preprocessing.",
+    "Albumentations for customized image augmentation.",
+    "Matplotlib and Seaborn for analysis and visualization.",
+    "Scikit-learn for performance evaluation metrics.",
+    "Carestream, 3D Slicer, and Dragonfly for 3D visualization of results."
+  ],
+}
+
 ];
 
 export default Details;
